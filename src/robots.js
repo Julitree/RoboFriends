54b6f213f450eb371e0e511de3f56b3dbc11ece9
Julia <julitreedesign@gmail.com>
2020-04-27 22:19:48 +0200

    robots js

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: "leanne Graham",
+        username: "Breat",
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: "ervin Howell",
+        username: "Antonette",
+        email: 'Shanna@lissa.tv'
+    },
+    {
+        id: 3,
+        name: "clementine Bauch",
+        username: "Samanta",
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: "patricia Bauch",
+        username: "Karianne",
+        email: 'Julianne@kory.org'
+    },
+    {
+        id: 5,
+        name: "bhelsey Dietrich",
+        username: "Kamren",
+        email: 'Happy@may.biz'
+    },
+    {
+        id: 6,
+        name: "olivia Graham",
+        username: "Oli",
+        email: 'Oligard@june.biz'
+    },
+    {
+        id: 7,
+        name: "justin Graham",
+        username: "Yoyo",
+        email: 'yoyo@march.biz'
+    },
+    {
+        id: 8,
+        name: "carlos Mina",
+        username: "Car",
+        email: 'Carlos@minina.biz'
+    },
+    {
+        id: 9,
+        name: "samanta Push",
+        username: "Sami",
+        email: 'Samantha@june.biz'
+    },
+    {
+        id: 10,
+        name: "lui Cameron",
+        username: "Lui",
+        email: 'LuiCam@june.biz'
+    },
+]
